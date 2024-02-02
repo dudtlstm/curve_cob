4af01b3fd0f0ec7ac677ddcb441060845069baf5
dudtlstm <annpys1024@naver.com>
2024-02-02 21:43:07 +0900

    feat: 랜딩페이지 하단부 완성

--- cob/src/pages/LandingPage.js
+++ cob/src/pages/LandingPage.js
@@ -8,6 +8,9 @@ import cautionImage from '../assets/images/body_image_caution.png';
 import chatImage from '../assets/images/body_image_chat.png';
 import ligthbulbImage from '../assets/images/body_image_lightbulb.png';
 import noticeImage from '../assets/images/body_image_notice.png';
+// import chattingImage from '../assets/images/body_image_chatting.png';
+// import choiceImage from '../assets/images/body_image_choice.png';
+import characteristicImage from '../assets/images/characteristic.png';
 
 export default function LandingPage() {
     return(
@@ -47,6 +50,38 @@ export default function LandingPage() {
                     사회생활 만렙 찍은 AI 직장인,<br/>
                     이 친구에게 직접 배우는 그 비결을 소개합니다.
                 </div>
+                <div className="body-left-text">
+                    지금 내가 연습해보고 싶은,<br/>
+                    어려움을 겪고 있는<br/>
+                    상황을 설정해보세요.
+                </div>
+                <div className="body-left-text">
+                    당신이 선택한 상황에 맞춰<br/>
+                    대화해볼 수 있어요
+                </div>
+                <div className="body-left-text">
+                    실수없이 말할 수 있도록<br/>
+                    커브와 함께 연습해볼 수 있어요
+                </div>
+        </div>
+        <div className="landing-container-bottom">
+            <div className="bottom-text">
+                이 외에도<br/>
+                커브는,
+            </div>
+            <div className="bottom-content">
+                {/* <div className="feature-box">
+                    <p>문서양식 공유</p>
+                    <h3>내용만 생각하세요,<br/>양식은 커브가 찾아올게요</h3>
+                    <img src={require('../assets/images/feature-icon1.png')} alt="특징 아이콘" />
+                </div>
+                <div className="feature-box">
+                    <p>스몰토크 주제 제안</p>
+                    <h3>어색한 자리에서,<br/>할 말이 없을 때</h3>
+                    <img src={require('../assets/images/feature-icon2.png')} alt="특징 아이콘" />
+                </div> */}
+                <img src={characteristicImage} alt="특징" className="characteristic"/>
+            </div>
         </div></>
     );
 }
